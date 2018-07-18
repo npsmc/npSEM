0c0318a64354ec472151ec62817cf68657dba238
Pierre Navaro <pierre.navaro@univ-rennes1.fr>
2018-07-18 17:47:00 +0200

    Rewrite methods.k_choice module

--- methods/k_choice.py
+++ methods/k_choice.py
@@ -1,128 +1,177 @@
-
-
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb 21 13:21:13 2018
 
 @author: trang
 """
+import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.pyplot as plt #plot
-#from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
-#from scipy.stats import multivariate_normal
+
 from methods.LLR_forecasting_CV import m_LLR
 
-def k_choice(LLR, x, y, time):
-    dx, N, T = x.shape;
-    if (LLR.Q.type == 'fixed') or (LLR.estK == 'same'):
-        L = np.zeros((len(LLR.nN_m),1));     E = np.zeros((len(LLR.nN_m),1))
 
-        for i in range(len(LLR.nN_m)):
-            LLR.k_m = LLR.nN_m[i]; LLR.k_Q = LLR.k_m;
-            loglik = 0; err =0;
-            for t in range(T):
-                _, mean_xf, Q_xf, _=  m_LLR(x[...,t],time[t],np.ones([1]),LLR);
-                innov = y[...,t]- mean_xf
-                err += np.mean((innov)**2) 
-                
-#                _, mean_xf, Q_xf, _=  m_LLR(x[:,t][np.newaxis].T,time[t],np.ones([1]),LLR);
-#                innov = y[:,t][np.newaxis].T- mean_xf
-#                
-#                loglik  += -.5 * np.log(2*np.pi*np.linalg.det(np.squeeze(Q_xf))) - .5 * innov.T.dot(np.linalg.inv(np.squeeze(Q_xf))).dot(innov)
-#                err += np.sqrt(np.mean((y[:,t][np.newaxis].T- mean_xf)**2))
+class Est:
+    """
+     type: chosen predefined type of model error covatiance ('fixed', 'adaptive')
+     form: chosen esimated matrix form ('full', 'diag', 'constant')
+     base: for fixed base of model covariance (for 'constant' matrix form only)
+     decision: chosen if Q is estimated or not ('True', 'False')
+    """
 
-               
-#                const = -.5 * np.log(2*np.pi*np.linalg.det(Q_xf.transpose(-1,0,1)))
-#                logwei = -.5*np.sum(innov.T.dot(np.linalg.inv(Q_xf.transpose(-1,0,1)))[np.arange(N),np.arange(N),:]*innov.T,1)
-#                
-#                loglik  +=  (np.sum(const + logwei))/N
-##                err += np.sqrt(np.mean((y[:,t][np.newaxis].T- mean_xf)**2))
-                
-            L[i] = loglik; E[i] = np.sqrt(err/T);
-                
-        ind_max = np.argmin(E); 
-        k_m = LLR.nN_m[ind_max]; 
-        k_Q = k_m
-        plt.rcParams['figure.figsize'] = (8, 5)        
-        plt.figure(3)
-        fig, ax1 = plt.subplots()
-        #ax2 = ax1.twinx()
-        ax1.plot(LLR.nN_m, E, color='b')
-        ax1.set_xlabel('number of $m$-analogs $(k_m)$')
-#        ax1.set_ylabel('log likelihood')
+    def __init__(self, value=None, type=None, form=None, base=None, decision=None):
+        self.value = value
+        self.type = type
+        self.form = form
+        self.base = base
+        self.decision = decision
 
-#        ax2.plot(LLR.nN_m, E, color='b')
-        ax1.set_ylabel('RMSE')
-#        plt.grid()
-        plt.show()
-    else:
-        X,Y = np.meshgrid(LLR.nN_m,LLR.nN_Q); Q = np.zeros((dx,dx,T));
-        len_ana = len(LLR.nN_m)*len(LLR.nN_Q)
-        L = np.zeros(len_ana); E = np.zeros(len_ana);
 
-        for i in range(len_ana):
-            LLR.k_m = np.squeeze(X.T.reshape(len_ana)[i]); 
-            if  np.squeeze(Y.T.reshape(len_ana)[i]) > LLR.k_m:
-                indY  =  divmod(i ,len(LLR.nN_Q));
-                Y[indY[1],indY[0]] =   LLR.k_m; # condition: number of analogs for m estimates is always larger or equal to the one for Q estimates
-            LLR.k_Q =np.squeeze(Y.T.reshape(len_ana)[i])
-            loglik = 0; err=0
-            for t in range(T):
-#                _, mean_xf, Q_xf, _=  m_LLR(x[:,t][np.newaxis].T,time[t],np.ones([1]),LLR);
-#                innov = y[:,t][np.newaxis].T- mean_xf
-#                loglik  += -.5 * np.log(2*np.pi*np.linalg.det(np.squeeze(Q_xf))) -.5 * innov.T.dot(np.linalg.inv(np.squeeze(Q_xf))).dot(innov)   
-#                err += np.mean((y[:,t][np.newaxis].T- mean_xf)**2)
-                _, mean_xf, Q_xf, _=  m_LLR(x[...,t],time[t],np.ones([1]),LLR);
-                innov = y[...,t]- mean_xf
-                const = -.5 * np.log(2*np.pi*np.linalg.det(Q_xf.transpose(-1,0,1)))
-                logwei = -.5*np.sum(innov.T.dot(np.linalg.inv(Q_xf.transpose(-1,0,1)))[np.arange(N),np.arange(N),:]*innov.T,1)
-                loglik  +=  (np.sum(const + logwei))/N
-                err += np.sqrt(np.mean((innov)**2)) 
- 
-                
-            L[i] = loglik; E[i] = np.sqrt(err/T)
-        ind_max= np.argmax(L)
-        k_m = np.squeeze(X.T.reshape(len_ana)[ind_max]);
-        k_Q = np.squeeze(Y.T.reshape(len_ana)[ind_max]);
-        print(L); print(E)
-        LL = (L.reshape((len(LLR.nN_m),len(LLR.nN_Q)))).T
-        plt.rcParams['figure.figsize'] = (9, 9)
-        fig = plt.figure(3)
 
-        ax = fig.gca(projection='3d')
-        surf = ax.plot_surface(X, Y, LL,cmap='Greys', alpha =0.75, linewidth=0.25,edgecolor='k', antialiased=False)
-#        plt.plot(k_m,k_Q,max(L),'k*', markersize = 6)
-        ax.zaxis.set_major_locator(LinearLocator(10))
-        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-        fig.colorbar(surf, shrink=0.5, aspect=5)
-        ax.set_xlabel('$k_m$') #number of $m$-analogs
-        ax.set_ylabel('$k_Q$') #number of $Q$-analogs
-        ax.set_zlabel('log likelihood')
-        plt.grid()
-        plt.show()
-        
-        
-        print('Q ={}'.format(np.mean(Q,2)))
-#        fig = plt.figure(4)
-#        plt.rcParams['figure.figsize'] = (5, 8)
-#        ax = fig.gca(projection='3d')
-#        surf = ax.plot_surface(X, Y, E.reshape((len(LLR.nN_m),len(LLR.nN_Q))), cmap='Blues',
-#                       linewidth=0, antialiased=False)
-#        ax.zaxis.set_major_locator(LinearLocator(10))
-#        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-#        fig.colorbar(surf, shrink=0.5, aspect=5)
-#        ax.set_xlabel('number of analogs $(k_m)$')
-#        ax.set_ylabel('number of analogs $(k_Q)$')
-#        ax.set_zlabel('RMSE')
-#        plt.grid()
-#        plt.show()
+class Data:
 
-        
-#        fig = plt.figure(4)
-#        plt.rcParams['figure.figsize'] = (5, 8)       
-# Add a color bar which maps values to colors.
+    def __init__(self, dx, data_init, ind_nogap, Y_train):
+        self.ana = np.zeros((dx, 1, len(ind_nogap)))
+        self.suc = np.zeros((dx, 1, len(ind_nogap)))
+        self.ana[:, 0, :] = data_init[:dx, ind_nogap]
+        self.suc[:, 0, :] = data_init[dx:, ind_nogap]
+        self.time = Y_train.time[ind_nogap]
 
-    return k_m, k_Q
+
+class LLRClass:
+    """
+    lagx:  lag of removed analogs around x to avoid an over-fitting forecast
+    lag_Dx:  15 lag of moving window of analogs chosen around x
+    time_period:  set 365.25 for year and
+    k_m: number of analogs
+    k_Q: number of analogs
+    nN_m:  number of analogs to be chosen for mean estimation
+    nN_Q: number of analogs to be chosen for dynamical error covariance
+    lag_k: chosen the lag for k reestimation
+    estK: set 'same' if k_m = k_Q chosen, otherwise, set 'different'
+    kernel: set 'rectangular' or 'tricube'
+    """
+    def __init__(self, data, data_prev, num_ana, Q):
+        self.data = data
+        self.data_prev = data_prev
+        self.lag_x = 5
+        self.lag_Dx = lambda Dx: np.shape(Dx)[-1]
+        self.time_period = 1
+        self.k_m = []
+        self.k_Q = []
+        self.nN_m = np.arange(20, num_ana, 50)
+        self.nN_Q = np.arange(20, num_ana, 50)
+        self.lag_k = 1
+        self.estK = 'same'
+        self.kernel = 'tricube'
+        self.k_lag = 20
+        self.k_inc = 10
+        self.Q = Q
+        self.gam = 1
+
+    def k_choice(self):
+        x = self.data.ana
+        y = self.data.suc
+        time = self.data.time
+        dx, N, T = x.shape
+        if self.Q.type == 'fixed' or self.estK == 'same':
+
+            print(" First case")
+
+            L = []
+            E = []
+
+            for i in range(len(self.nN_m)):
+                self.k_m = self.nN_m[i]
+                self.k_Q = self.k_m
+                loglik = 0
+                err = 0
+                for t in range(T):
+                    _, mean_xf, Q_xf, _ = m_LLR(x[..., t], time[t], np.ones([1]), self)
+                    innov = y[..., t] - mean_xf
+                    err += np.mean(innov ** 2)
+
+                L.append(loglik)
+                E.append(np.sqrt(err / T))
+
+            ind_max = np.argmin(E)
+            k_m = self.nN_m[ind_max]
+            k_Q = k_m
+            plt.rcParams['figure.figsize'] = (8, 5)
+            plt.figure(3)
+            fig, ax1 = plt.subplots()
+            ax1.plot(self.nN_m, E, color='b')
+            ax1.set_xlabel('number of $m$-analogs $(k_m)$')
+            ax1.set_ylabel('RMSE')
+            plt.show()
+        else:
+            X, Y = np.meshgrid(self.nN_m, self.nN_Q)
+            Q = np.zeros((dx, dx, T))
+            len_ana = len(self.nN_m) * len(self.nN_Q)
+            L = np.zeros(len_ana)
+            E = np.zeros(len_ana)
+
+            for i in range(len_ana):
+                self.k_m = np.squeeze(X.T.reshape(len_ana)[i])
+                if np.squeeze(Y.T.reshape(len_ana)[i]) > self.k_m:
+                    indY = divmod(i, len(self.nN_Q))
+
+                    # condition: number of analogs for m estimates is
+                    # always larger or equal to the one for Q estimates
+                    Y[indY[1], indY[0]] = self.k_m
+                self.k_Q = np.squeeze(Y.T.reshape(len_ana)[i])
+                loglik = 0
+                err = 0
+                for t in range(T):
+                    _, mean_xf, Q_xf, _ = m_LLR(x[..., t], time[t], np.ones([1]), self)
+                    innov = y[..., t] - mean_xf
+                    const = -.5 * np.log(2 * np.pi * np.linalg.det(Q_xf.transpose(-1, 0, 1)))
+                    logwei = -.5 * np.sum(
+                        innov.T.dot(np.linalg.inv(Q_xf.transpose(-1, 0, 1)))[:N, :N, :] * innov.T, 1)
+                    loglik += np.sum(const + logwei) / N
+                    err += np.sqrt(np.mean(innov ** 2))
+
+                L[i] = loglik
+                E[i] = np.sqrt(err / T)
+            ind_max = np.argmax(L)
+            self.k_m = np.squeeze(X.T.reshape(len_ana)[ind_max])
+            self.k_Q = np.squeeze(Y.T.reshape(len_ana)[ind_max])
+            print(L)
+            print(E)
+            LL = (L.reshape((len(self.nN_m), len(self.nN_Q)))).T
+            plt.rcParams['figure.figsize'] = (9, 9)
+            fig = plt.figure(3)
+
+            ax = fig.gca(projection='3d')
+            surf = ax.plot_surface(X, Y, LL, cmap='Greys', alpha=0.75,
+                                   linewidth=0.25, edgecolor='k', antialiased=False)
+            #        plt.plot(k_m,k_Q,max(L),'k*', markersize = 6)
+            ax.zaxis.set_major_locator(LinearLocator(10))
+            ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+            fig.colorbar(surf, shrink=0.5, aspect=5)
+            ax.set_xlabel('$k_m$')  # number of $m$-analogs
+            ax.set_ylabel('$k_Q$')  # number of $Q$-analogs
+            ax.set_zlabel('log likelihood')
+            plt.grid()
+            plt.show()
+
+            print('Q ={}'.format(np.mean(Q, 2)))
+        #        fig = plt.figure(4)
+        #        plt.rcParams['figure.figsize'] = (5, 8)
+        #        ax = fig.gca(projection='3d')
+        #        surf = ax.plot_surface(X, Y, E.reshape((len(LLR.nN_m),len(LLR.nN_Q))), cmap='Blues',
+        #                       linewidth=0, antialiased=False)
+        #        ax.zaxis.set_major_locator(LinearLocator(10))
+        #        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+        #        fig.colorbar(surf, shrink=0.5, aspect=5)
+        #        ax.set_xlabel('number of analogs $(k_m)$')
+        #        ax.set_ylabel('number of analogs $(k_Q)$')
+        #        ax.set_zlabel('RMSE')
+        #        plt.grid()
+        #        plt.show()
+
+        #        fig = plt.figure(4)
+        #        plt.rcParams['figure.figsize'] = (5, 8)
+        # Add a color bar which maps values to colors.
+
---
+++ test_L63.py
@@ -0,0 +1,124 @@
+from time import time
+
+import numpy as np
+
+import models.l63f as mdl_l63
+from methods.CPF_BS_smoothing import _CPF
+from methods.LLR_forecasting_CV import m_LLR
+from methods.generate_data import generate_data
+from methods.k_choice import LLRClass, Data, Est
+from methods.model_forecasting import m_true
+from models.L63 import l63_jac
+
+begin = time()
+"""
+ GENERATE SIMULATED DATA (LORENZ-63 MODEL)
+ 
+ dx                 : dimension of the state
+ dt_int             : fixed integration time
+ dt_model           : chosen number of model time step  \in [1, 25]
+                      the larger dt_model the more nonlinear model
+ var_obs            : indices of the observed variables
+ dy                 : dimension of the observations
+ H                  : first and third variables are observed
+ h                  : observation model
+ jacH               : Jacobian matrix  of the observation model(for EKS_EM only)
+ sigma, rho, beta   : physical parameters
+ Q_true             : model covariance
+ R_true             : observation covariance
+"""
+dx = 3
+dt_int = 0.01
+dt_model = 8
+var_obs = np.array([0, 1, 2])
+dy = len(var_obs)
+H = np.eye(dx)  # H = H[(0,2),:]
+h = lambda x: H.dot(x)
+jacH = lambda x: H
+sigma = 10.0
+rho = 28.0
+beta = 8.0 / 3
+fmdl = mdl_l63.M(sigma=sigma, rho=rho, beta=beta, dtcy=dt_int)
+mx = lambda x: fmdl.integ(x)  # fortran version (fast)
+jac_mx = lambda x: l63_jac(x, dt_int * dt_model, sigma, rho, beta)  # python version (slow)
+
+# Setting covariances
+sig2_Q = 1;
+sig2_R = 2  # parameters
+Q_true = np.eye(dx) * sig2_Q
+R_true = np.eye(dx) * sig2_R
+
+# prior state
+x0 = np.r_[8, 0, 30]
+
+# generate data
+T_burnin = 5 * 10 ** 3
+T_train = 10 * 10 ** 2  # length of the catalog
+T_test = 10 * 10 ** 2  # length of the testing data
+X_train, Y_train, X_test, Y_test, yo = generate_data(x0, mx, h, Q_true,
+                                                     R_true, dt_int, dt_model,
+                                                     var_obs, T_burnin,
+                                                     T_train, T_test)
+X_train.time = np.arange(0, T_train)
+Y_train.time = X_train.time[1:]
+# np.random.seed(0);# random number generator
+N = np.size(Y_train.values)
+Ngap = N // 10  # create gaps: 10 percent of missing values
+indX = np.random.choice(np.arange(0, N), Ngap, replace=False)
+ind_gap_taken = divmod(indX, len(Y_train.time))
+Y_train.values[ind_gap_taken] = np.nan
+
+X_train0, Y_train0, X_test0, Y_test0, yo0 = generate_data(X_train.values[:, -1],
+                                                          mx, h, Q_true, R_true,
+                                                          dt_int,
+                                                          dt_model, var_obs, T_burnin,
+                                                          T_train, T_test)
+X_train0.time = np.arange(0, T_train)
+Y_train0.time = X_train0.time[1:]
+
+# %% TEST FOR LOCAL LINEAR REGRESSION (LLR_forecasting_CV function)
+data_init = np.r_['0,2,0', Y_train.values[..., :-1], Y_train.values[..., 1:]]
+ind_nogap = np.where(~np.isnan(np.sum(data_init, 0)))[0]
+num_ana = 200
+
+estQ =  Est(value=Q_true,type='adaptive',form='constant',base=np.eye(dx),decision=True)
+estR =  Est(value=R_true,type='adaptive',form='constant',base=np.eye(dx),decision=True)
+estX0 = Est(decision = False)
+estD =  Est(decision = True)
+
+data = Data(dx, data_init, ind_nogap, Y_train)
+data_prev = Data(dx, data_init, ind_nogap, Y_train)
+
+data_prev.ana = data_init[:dx, ind_nogap]
+data_prev.suc = data_init[dx:, ind_nogap]
+data_prev.time = Y_train.time[ind_nogap]
+
+LLR = LLRClass(data, data_prev, num_ana, estQ)
+
+LLR.k_choice()
+
+print(f"Elapsed time : {time()-begin}")
+
+# Forecast with LLR
+xf, mean_xf, Q_xf, M_xf = m_LLR(X_test.values[:, :-1], 1, np.ones([1]), LLR)  # Need to be optimised
+
+# %% TEST FOR CONDITIONAL PARTICLE FILTERING (_CPF function)
+# Here I use the true model  (m) to forecast instead of LLR (m_hat).
+
+m = lambda x, pos_x, ind_x: m_true(x, pos_x, ind_x, Q_true, mx, jac_mx, dt_model)
+# m_hat = lambda  x,pos_x,ind_x: m_LLR(x,pos_x,ind_x,LLR)
+
+time = np.arange(T_test - 1)  # [np.newaxis].T
+B = Q_true
+xb = X_test.values[..., 0]
+
+Nf = 10  # number of particles
+Ns = 5  # number of realizations
+
+X_conditioning = np.zeros([dx, T_test])
+
+Xa, Xf, mean_Xf, cov_Xf, Wa, ind, loglik = _CPF(Y_test.values,
+                                                X_conditioning,
+                                                m, H, Q_true, R_true,
+                                                xb, B, Nf, dx,
+                                                time)  # Need to be optimised
