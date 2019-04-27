0cc94d7c8a537740a5c971a7310b094136b63835
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-04-27 16:51:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import find_packages
 from numpy.distutils.core import setup, Extension
+
 import os
 
 l63_model = Extension(name="noisette.models.l63_for", 
-                      sources = [os.path.join("lorenz63","models","l63_for.f90")])
+                      sources = [os.path.join("noisette","models","l63_for.f90")])
 l96_model = Extension(name="noisette.models.l96_for", 
-                      sources = [os.path.join("lorenz63","models","l96_for.f90")],
+                      sources = [os.path.join("noisette","models","l96_for.f90")],
                       f2py_options=['only:', 'tinteg1scl emtinteg1scl', ':'])
 
 setup(name        = "noisette",
@@ -14,4 +15,10 @@ setup(name        = "noisette",
       author      = "Thi Tuyet Trang Chau",
       author_email= "thi-tuyet-trang.chau@univ-rennes1.fr",
       url         = "https://gitlab.univ-rennes1.fr/wind/lorenz63",
+      install_requires = ['numpy', 'scipy'],
+      packages    = find_packages(exclude=['notebooks', 
+                                           'doc', 
+                                           'examples', 
+                                           'test_*',
+                                           '*.f90']),
       ext_modules = [l63_model, l96_model] )
