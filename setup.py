3383e058faaaf071b11a5c931773f63ed69af859
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-04-27 16:25:40 +0200

    change package name to noisette

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import find_packages
 from numpy.distutils.core import setup, Extension
 import os
 
-l63_model = Extension(name="lorenz63.models.l63_for", 
+l63_model = Extension(name="noisette.models.l63_for", 
                       sources = [os.path.join("lorenz63","models","l63_for.f90")])
-l96_model = Extension(name="lorenz63.models.l96_for", 
+l96_model = Extension(name="noisette.models.l96_for", 
                       sources = [os.path.join("lorenz63","models","l96_for.f90")],
                       f2py_options=['only:', 'tinteg1scl emtinteg1scl', ':'])
 
-setup(name        = "lorenz63",
+setup(name        = "noisette",
       version     = "0.1.0",
-      description = "A non-parametric algorithm for reconstruction and estimation in nonlinear time series with observational errors",
+      description = "A Non-parametric algorithm for reconstruction and estimation in nonlinear time series with observational errors",
       author      = "Thi Tuyet Trang Chau",
       author_email= "thi-tuyet-trang.chau@univ-rennes1.fr",
       url         = "https://gitlab.univ-rennes1.fr/wind/lorenz63",
