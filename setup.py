7f5ec93990c888fcbea370f04ffffe8f48ec97fc
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-04-27 16:10:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import find_packages
 from numpy.distutils.core import setup, Extension
+import os
 
-l63_model = Extension(name="l63_for", sources = "lorenz63/models/l63_for.f90")
-l96_model = Extension(name="l96_for", sources = "lorenz63/models/l96_for.f90")
+l63_model = Extension(name="lorenz63.models.l63_for", 
+                      sources = [os.path.join("lorenz63","models","l63_for.f90")])
+l96_model = Extension(name="lorenz63.models.l96_for", 
+                      sources = [os.path.join("lorenz63","models","l96_for.f90")],
+                      f2py_options=['only:', 'tinteg1scl emtinteg1scl', ':'])
 
 setup(name        = "lorenz63",
       version     = "0.1.0",
@@ -10,5 +14,4 @@ setup(name        = "lorenz63",
       author      = "Thi Tuyet Trang Chau",
       author_email= "thi-tuyet-trang.chau@univ-rennes1.fr",
       url         = "https://gitlab.univ-rennes1.fr/wind/lorenz63",
-      packages    = find_packages(),
       ext_modules = [l63_model, l96_model] )
