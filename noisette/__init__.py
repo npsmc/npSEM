73d2797e79175738d7dc116d31019a9a4ce00760
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-04-28 17:02:39 +0200

    Update __init__.py

--- noisette/__init__.py
+++ noisette/__init__.py
@@ -1,4 +1,10 @@
+import sys
+
 try:
     import noisette.models.l63f as mdl_l63
-except ImportError:
-    print("Do not forget to build fortran modules")
+except ModuleNotFoundError:
+    print("""\
+          Do not forget to build fortran modules
+          run `python3 setup.py build_ext --inplace`
+          """)
+    sys.exit(0)
