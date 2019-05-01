4ebf34001cb6400946c55f68ba8a4dfd4ada5173
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-05-01 18:05:04 +0200

    Update test_generate_data_lorenz63.py

--- test_generate_data_lorenz63.py
+++ test_generate_data_lorenz63.py
@@ -6,15 +6,26 @@ import npsem.models.l63f as mdl_l63
 from   npsem.models.l63 import l63_jac
  
 
-sigma  = 10.0,
-rho    = 28.0,
-beta   = 8.0 / 3
-h      = lambda x : H.dot(x)
-jac_h  = lambda x : H
-mx     = lambda x : fmdl.integrate(x)
-jac_mx = lambda x, dt : l63_jac(x, dt, sigma, rho, beta)  
+dt_int   = 0.01
+dt_model = 8
+var_obs  = [0, 1, 2]
+sig2_Q   = 1
+sig2_R   = 2 
+x0       = [8, 0, 30]
 
-lorenz63 = SSM(h = h, jac_h = jac_h, mx = mx, jac_mx = jac_mx)
+dx       = len(x0)
+H        = np.eye(dx)
+h        = lambda x : H.dot(x)
+jac_h    = lambda x : H
+
+sigma, rho, beta    = 10.0, 28.0, 8.0 / 3
+fmdl     = mdl_l63.M(sigma, rho, beta, dt_int)
+mx       = lambda x : fmdl.integrate(x)
+jac_mx   = lambda x, dt : l63_jac(x, dt, sigma, rho, beta)  
+
+lorenz63 = SSM(h, jac_h, mx, jac_mx, dt_int, dt_model, x0, var_obs, 
+               sig2_Q, sig2_R)
+
 T_burnin = 5  * 10**3
 T        = 20 * 10**2
 X, Y     = lorenz63.generate_data( T_burnin, T)
