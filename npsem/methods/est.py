a1d2e76762fd5f3f91aed1987deb713366aee4f4
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-05-01 21:15:10 +0200

    Update est.py

--- npsem/methods/est.py
+++ npsem/methods/est.py
@@ -2,6 +2,7 @@ __author__ = "Pierre Navaro"
 __email__ = "navaro@math.cnrs.fr"
 
 class Est:
+
     """
      type: chosen predefined type of model error covatiance ('fixed', 'adaptive')
      form: chosen esimated matrix form ('full', 'diag', 'constant')
@@ -10,8 +11,12 @@ class Est:
     """
 
     def __init__(self, value=None, type=None, form=None, base=None, decision=None):
+
         self.value = value
+        assert type in ["fixed", "adaptative", None]
         self.type = type
+        assert form in ['full', 'diag', 'constant', None]
         self.form = form
         self.base = base
+        assert type(decision) is bool
         self.decision = decision
