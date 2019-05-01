2cf7c92c00bfea4d6d41cfe049026653bf860bfa
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-05-01 22:14:26 +0200

    Update data.py

--- npsem/methods/data.py
+++ npsem/methods/data.py
@@ -5,9 +5,11 @@ import numpy as np
 
 class Data:
 
-    def __init__(self, dx, data_init, ind_nogap, Y_train):
-        self.ana = np.zeros((dx, 1, len(ind_nogap)))
-        self.suc = np.zeros((dx, 1, len(ind_nogap)))
+    def __init__(self, dx, data_init, Y):
+
+        ind_nogap = np.where(~np.isnan(np.sum(data_init,0)))[0]; 
+        self.ana  = np.zeros((dx, 1, len(ind_nogap)))
+        self.suc  = np.zeros((dx, 1, len(ind_nogap)))
         self.ana[:, 0, :] = data_init[:dx, ind_nogap]
         self.suc[:, 0, :] = data_init[dx:, ind_nogap]
-        self.time = Y_train.time[ind_nogap]
+        self.time = Y.time[ind_nogap]
