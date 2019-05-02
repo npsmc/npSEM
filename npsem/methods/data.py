4da9102a2adb4b52a093aedc92d3e2f57a2895f0
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-05-02 22:27:48 +0200

    Update data.py

--- npsem/methods/data.py
+++ npsem/methods/data.py
@@ -7,7 +7,7 @@ class Data:
 
     def __init__(self, dx, data_init, Y):
 
-        ind_nogap = np.where(~np.isnan(np.sum(data_init,0)))[0]; 
+        ind_nogap = np.where(~np.isnan(np.sum(data_init,0)))[0] 
         self.ana  = np.zeros((dx, 1, len(ind_nogap)))
         self.suc  = np.zeros((dx, 1, len(ind_nogap)))
         self.ana[:, 0, :] = data_init[:dx, ind_nogap]
